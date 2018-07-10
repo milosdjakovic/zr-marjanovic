026e533950908776f18b25a973770fb4669a4886
MilosDjakovic <milosdjakovic@users.noreply.github.com>
2018-07-10 09:59:09 +0200

    fixed carousel dots and text in Kontakt.jsx

--- src/Content/Kontakt/Kontakt.jsx
+++ src/Content/Kontakt/Kontakt.jsx
@@ -6,7 +6,7 @@ import './Kontakt.css';
 const Kontakt = () => {
     const contactData = [
         {
-            type: 'Zanarska Radionica',
+            type: 'Zanatska Radionica',
             title: 'Marjanović',
             place: 'Subotinac',
             map: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1444.619132529447!2d21.697126657364887!3d43.60158039474055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDPCsDM2JzA1LjciTiAyMcKwNDEnNTIuNSJF!5e0!3m2!1sen!2srs!4v1530700858068'
